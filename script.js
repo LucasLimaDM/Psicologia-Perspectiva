8f9bd7070e392b7e074dc64aa7857a9ecdc38028
LucasLimaDM <lucaslim.dias@gmail.com>
2023-02-18 09:12:39 -0300

    Estrutura do menu horizontal

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 
-let navHeader = document.querySelector('#button-container')
+let ulHeader = document.querySelector('#button-container ul')
 let sidebar = document.querySelector('#sidebar')
+let ulSidebar = document.querySelector('#sidebar ul')
 let menu = document.querySelector('#menu')
-sidebar.innerHTML = navHeader.innerHTML;
+ulSidebar.innerHTML += ulHeader.innerHTML;
+let menuSidebar = document.querySelector('#menu-sidebar')
+
 
 // Abre e fecha o menu
 menu.addEventListener('click', function(){
+    console.log('menu!')
     listenerLink()
-    if (sidebar.classList.contains('aberto')) {
-        sidebar.classList.remove('aberto')
-    } else {
-        sidebar.classList.add('aberto')
-    }
+    sidebar.classList.add('aberto')
 })
-
+menuSidebar.addEventListener('click', function(){
+    console.log('menu close')
+    listenerLink()
+    sidebar.classList.remove('aberto')
+})
 
 // chama a função scrollNaTela ao clicar em cada link 
 function listenerLink() {
-    let links = document.querySelectorAll('#sidebar a');
-    console.log("🚀 ~ file: script.js:21 ~ listenerLink ~ links", links)
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', function() {scrollNaTela(i)})
+    let linksHeader = document.querySelectorAll('header a');
+    console.log("🚀 ~ file: script.js:21 ~ listenerLink ~ links", linksHeader)
+    for (let i = 0; i < linksHeader.length; i++) {
+        linksHeader[i].addEventListener('click', function() {scrollNaTela(i)})
+    }
+
+    let linksSidebar = document.querySelectorAll('#sidebar a');
+    console.log("🚀 ~ file: script.js:21 ~ listenerLink ~ links", linksSidebar)
+    for (let i = 0; i < linksSidebar.length; i++) {
+        linksSidebar[i].addEventListener('click', function() {scrollNaTela(i)})
     }
 }
 
@@ -30,7 +40,7 @@ function scrollNaTela(numPosition) {
 
     let targetOffset = $(trataPosition(numPosition)).offset().top
     let targetPosition = parseInt(targetOffset)-70
-    $('html,body').animate({scrollTop: targetPosition}, 500)
+    $('html,body').animate({scrollTop: targetPosition}, 400)
 }
 
 // troca o valor numérico de i pelo respectivo indicador jquery
@@ -47,4 +57,10 @@ function trataPosition(number) {
         default:
             console.error('switch recebendo valor invalido')
     }
+}
+
+document.body.onresize = function(){
+    if(sidebar.classList.contains('aberto')){
+        sidebar.classList.remove('aberto')
+    }   
 }
